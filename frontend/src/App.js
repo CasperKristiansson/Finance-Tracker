5f4ff79355e2335a5b2c6e6a286df69f6ad91b4d
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-07-03 09:12:16 +0200

    Basic sidebar layout template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './sidebar/sidebar.scss';
+
+import Sidebar from './sidebar/sidebar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Sidebar />
     </div>
   );
 }
