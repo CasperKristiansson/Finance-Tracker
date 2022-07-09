49d3de23708932e26c0ad15cbcefbaa2127a7982
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-07-09 14:58:02 +0200

    Move pie chart data to separate file

--- frontend/src/graphs/piechart.js
+++ frontend/src/graphs/piechart.js
@@ -1,14 +1,76 @@
+import React from 'react';
 import { Pie } from 'react-chartjs-2';
 
 export default (props) => {
+    const [data, setData] = React.useState({
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [],
+        borderColor: [],
+        borderWidth: 0,
+      },
+    ],
+  });
+
+  var backgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+  ]
+  var borderColor = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)',
+  ]
+  var hoverBackgroundColor = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)',
+  ]
+  var hoverBorderColor = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)',
+  ]
+  var borderWidth = 1
+
+  React.useEffect(() => {
+    setData({
+      labels: props.labels,
+      datasets: [
+        {
+          data: props.data,
+          backgroundColor: backgroundColor,
+          borderColor: borderColor,
+          borderWidth: borderWidth,
+          hoverBorderColor: hoverBorderColor,
+          hoverBackgroundColor: hoverBackgroundColor,
+        },
+      ],
+    });
+  }, [props.data, props.title]);
+
 	return(
 		<>
 			<h2>
 				{props.title}
 			</h2>
 			<Pie
-				data={props.data}
-				options={props.options}
+				data={data}
 			/>
 		</>
 	);
--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -60,7 +60,7 @@ export default () => {
 															backgroundColor: "rgba(255,99,132,0.2)",
 															borderColor: "rgba(255,99,132,1)",
 															borderWidth: 1,
-															hoverBackgroundColor: "rgba(255,99,132,0.4)",
+															hoverBackgroundColor: "rgba(255,99,132,1)",
 															hoverBorderColor: "rgba(255,99,132,1)",
 															data: [65, 59, 80, 81, 56, 55, 40, 80, 81, 56, 55, 40]
 														},
@@ -69,7 +69,7 @@ export default () => {
 															backgroundColor: "rgba(54,162,235,0.2)",
 															borderColor: "rgba(54,162,235,1)",
 															borderWidth: 1,
-															hoverBackgroundColor: "rgba(54,162,235,0.4)",
+															hoverBackgroundColor: "rgba(54,162,235,1)",
 															hoverBorderColor: "rgba(54,162,235,1)",
 															data: [65, 59, 80, 81, 56, 55, 40, 80, 81, 56, 55, 40]
 														}
@@ -115,35 +115,8 @@ export default () => {
 									<div className={"main-section-pie"}>
 										<PieChart 
 											title="Expenses"
-											data={{
-												labels: ["Food (25%)", "Transport", "Entertainment", "Other"],
-												datasets: [
-													{
-														label: "Expenses",
-														data: [12, 19, 3, 5],
-														backgroundColor: [
-															'rgba(255, 99, 132, 0.2)',
-															'rgba(54, 162, 235, 0.2)',
-															'rgba(255, 206, 86, 0.2)',
-															'rgba(75, 192, 192, 0.2)',
-															'rgba(153, 102, 255, 0.2)',
-															'rgba(255, 159, 64, 0.2)',
-														],
-														borderColor: [
-															'rgba(255, 99, 132, 1)',
-															'rgba(54, 162, 235, 1)',
-															'rgba(255, 206, 86, 1)',
-															'rgba(75, 192, 192, 1)',
-															'rgba(153, 102, 255, 1)',
-															'rgba(255, 159, 64, 1)',
-														],
-														borderWidth: 1,
-													}
-												]
-											}}
-											options={{
-												
-											}}		
+											labels={["Food (25%)", "Transport", "Entertainment", "Other"]}
+											data={[12, 19, 3, 5]}
 										/>
 									</div>
 								</Segment>
