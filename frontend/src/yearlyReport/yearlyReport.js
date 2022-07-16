55ce54a7ecd86449c1a976f318a47f456b00c6d1
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-07-16 16:23:58 +0200

    Basic pie chart of yearly report

--- frontend/src/yearlyReport/yearlyReport.js
+++ frontend/src/yearlyReport/yearlyReport.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Segment, Button } from "semantic-ui-react";
 import BarChart from "../graphs/barchart";
 import LineChart from "../graphs/linechart";
+import PieChart from "../graphs/piechart";
 import { transactions } from "../data.js";
 
 export default (props) => {
@@ -27,6 +28,32 @@ export default (props) => {
 								<LineChart
 									title={`Wealth Growth for ${year}`}
 									data={calculateNetWorthIncrease(transactions, year)}
+								/>
+							</Segment>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+				<Grid columns={3}>
+					<Grid.Row stretched>
+						<Grid.Column>
+							<Segment>
+								<PieChart
+									title={`Income`}
+									labels={getTransactionCategories(transactions, year, "Income")}
+									data={getTransactionAmounts(transactions, year, "Income")}
+								/>
+							</Segment>
+						</Grid.Column>
+						<Grid.Column>
+							<Segment>
+							</Segment>
+						</Grid.Column>
+						<Grid.Column>
+							<Segment>
+								<PieChart
+									title={`Expense`}
+									labels={getTransactionCategories(transactions, year, "Expense")}
+									data={getTransactionAmounts(transactions, year, "Expense")}
 								/>
 							</Segment>
 						</Grid.Column>
@@ -93,4 +120,49 @@ function calculateNetWorthIncrease(transactions, year) {
 	}
 
 	return netWorthIncrease;
+}
+
+function getTransactionCategories(transactions, year, type) {
+	// get all of the different categories for the given year by storing it in a map, and only keep the type
+	let map = new Map();
+	transactions.forEach(transaction => {
+		var currentDate = new Date(transaction.Date);
+		if (transaction.Type == type && currentDate.getFullYear() == year) {
+			if (map.has(transaction.Category)) {
+				map.set(transaction.Category, map.get(transaction.Category) + 1);
+			}
+			else {
+				map.set(transaction.Category, 1);
+			}
+		}
+	});
+
+	let categories = [];
+	map.forEach((value, key) => {
+		categories.push(key);
+	});
+
+	return categories;
+}
+
+function getTransactionAmounts(transactions, year, type) {
+	let map = new Map();
+	transactions.forEach(transaction => {
+		var currentDate = new Date(transaction.Date);
+		if (transaction.Type == type && currentDate.getFullYear() == year) {
+			if (map.has(transaction.Category)) {
+				map.set(transaction.Category, map.get(transaction.Category) + transaction.Amount);
+			}
+			else {
+				map.set(transaction.Category, transaction.Amount);
+			}
+		}
+	});
+
+	let amounts = [];
+	map.forEach((value, key) => {
+		amounts.push(value);
+	});
+
+	return amounts;
 }
