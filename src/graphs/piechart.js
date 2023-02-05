a83afeaf0f536c1b3de7d75630731d9c3d991907
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-05 13:55:43 +0100

    Piechart warnings

--- src/graphs/piechart.js
+++ src/graphs/piechart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
 
-export default (props) => {
+const PieChart = (props) => {
     const [data, setData] = React.useState({
     labels: [],
     datasets: [
@@ -14,41 +14,41 @@ export default (props) => {
     ],
   });
 
-  var backgroundColor = [
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-  ]
-  var borderColor = [
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 99, 132, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-  ]
-  var hoverBackgroundColor = [
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 99, 132, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-  ]
-  var hoverBorderColor = [
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 99, 132, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-  ]
-  var borderWidth = 1
-
   React.useEffect(() => {
+    var backgroundColor = [
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+    ]
+    var borderColor = [
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 99, 132, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+    ]
+    var hoverBackgroundColor = [
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 99, 132, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+    ]
+    var hoverBorderColor = [
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 99, 132, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+    ]
+    var borderWidth = 1
+
     setData({
       labels: props.labels,
       datasets: [
@@ -62,7 +62,7 @@ export default (props) => {
         },
       ],
     });
-  }, [props.data, props.title]);
+  }, [props.data, props.labels, props.title]);
 
 	return(
 		<>
@@ -75,3 +75,5 @@ export default (props) => {
 		</>
 	);
 }
+
+export default PieChart;
