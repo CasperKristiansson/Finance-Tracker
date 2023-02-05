5b02959ab5d8c02ec559f825c21911d59f84daaa
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-05 13:45:04 +0100

    Milestones warnings

--- src/milestones/milestones.js
+++ src/milestones/milestones.js
@@ -1,42 +1,33 @@
 import React, {useEffect, useState} from "react";
-import { Grid, Segment, Divider, Button, Icon, Header } from "semantic-ui-react";
+import { Grid, Segment, Divider, Header } from "semantic-ui-react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 import './milestones.css';
 
 
-export default (props) => {
+const Milestones = (props) => {
 	const [milestones, setMilestones] = useState([]);
 	const [transactions, setTransactions] = useState([]);
 	const [loans, setLoans] = useState([]);
 	
-	var loadedTransactions = false;
-
-	let navigate = useNavigate();
-
 	useEffect(() => {
-		if (!loadedTransactions) {
-			var params = new URLSearchParams();
-			params.append('userID', props.userID);
+		var params = new URLSearchParams();
+		params.append('userID', props.userID);
 
-			axios.post('https://pktraffic.com/api/transactionsTotal.php', params).then(response => {
-				console.log(response.data);
-				setTransactions(response.data.transactions);
-			}).catch(response => {
-				console.log(response);
-			});
+		axios.post('https://pktraffic.com/api/transactionsTotal.php', params).then(response => {
+			console.log(response.data);
+			setTransactions(response.data.transactions);
+		}).catch(response => {
+			console.log(response);
+		});
 
-			axios.post('https://pktraffic.com/api/loans.php', params).then(response => {
-				console.log(response.data);
-				setLoans(response.data.transactions);
-			}).catch(response => {
-				console.log(response);
-			});
-
-			loadedTransactions = true;
-		}
-	}, []);
+		axios.post('https://pktraffic.com/api/loans.php', params).then(response => {
+			console.log(response.data);
+			setLoans(response.data.transactions);
+		}).catch(response => {
+			console.log(response);
+		});
+	}, [props.userID]);
 
 	useEffect(() => {
 		if (transactions.length > 0 && loans.length > 0) {
@@ -145,9 +136,9 @@ function calculateMilestones(transactions) {
 	var total = 0;
 	var date = transactions[0].Date;
 	for (var i = 0; i < transactions.length; i++) {
-		if (transactions[i].Type == "Income") {
+		if (transactions[i].Type === "Income") {
 			total += parseInt(transactions[i].Amount);
-		} else if (transactions[i].Type == "Expense") {
+		} else if (transactions[i].Type === "Expense") {
 			total -= parseInt(transactions[i].Amount);
 		} else {
 			continue;
@@ -159,13 +150,13 @@ function calculateMilestones(transactions) {
 				milestones[milestone].achievedDate = formatDate(transactions[i].Date);
 				milestones[milestone].achievedTime = calculateTime(date, transactions[i].Date);
 
-				var date = transactions[i].Date;
+				date = transactions[i].Date;
 			}
 		}
 	}
 
 	var milestonesArray = [];
-	for (var milestone in milestones) {
+	for (milestone in milestones) {
 		milestonesArray.push({
 			title: milestone,
 			achieved: milestones[milestone].achieved,
@@ -178,8 +169,8 @@ function calculateMilestones(transactions) {
 }
 
 function calculateTime(date1, date2) {
-	var date1 = new Date(date1);
-	var date2 = new Date(date2);
+	date1 = new Date(date1);
+	date2 = new Date(date2);
 
 	var timeDiff = Math.abs(date2.getTime() - date1.getTime());
 	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
@@ -190,3 +181,5 @@ function calculateTime(date1, date2) {
 function formatDate(date) {
 	return date.split(" ")[0];
 }
+
+export default Milestones;
