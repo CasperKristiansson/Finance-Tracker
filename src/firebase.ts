05c4e76febe79f7e093e47a867a903be21da6b91
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-07 09:32:28 +0100

    Convert firebase to configs

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDT6jddGYM4TRKBB3fEmdKJkuLk6IdwGXg",
   authDomain: "financetrackerprivate.firebaseapp.com",
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: "1:404676186515:web:6c22e27005b5add4999866"
 };
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
