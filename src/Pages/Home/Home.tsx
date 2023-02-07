7d69a6a575c5e1a89c53896b899bdb757d69bdb5
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-07 10:17:16 +0100

    Add message box

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,67 @@
 import React, {useEffect, useState} from "react";
+import { createUseStyles } from "react-jss";
 
 import { HomeHeader } from './ChildComponents/HomeHeader';
 
-import { getStartPeriod, MonthYear } from '../../Utils/Date';
+import { getStartPeriod } from '../../Utils/Date';
+
+// Convert to jss
+// .main-section {
+// 	padding-top: 0px;
+// 	margin-left: 270px;
+// 	padding: 25px 25px;
+// }
+
+// .main-section-pie {
+// 	margin-left: 25%;
+// 	margin-right: 25%;
+// }
+
+// .main-section-button {
+// 	width: 100%;
+// 	height: 70px;
+// }
+
+// .grid-max-height {
+// 	height: 100%;
+// }
+
+// .message-sticky {
+// 	position: fixed;
+// 	top: 25px;
+// 	right: 25px;
+// 	width: 500px;
+// 	z-index: 999;
+// }
+const useStyles = createUseStyles({
+	mainSection: {
+		paddingTop: 0,
+		marginLeft: 270,
+		padding: "25px 25px",
+	},
+	mainSectionPie: {
+		marginLeft: "25%",
+		marginRight: "25%",
+	},
+	mainSectionButton: {
+		width: "100%",
+		height: 70,
+	},
+	gridMaxHeight: {
+		height: "100%",
+	},
+	messageSticky: {
+		position: "fixed",
+		top: 25,
+		right: 25,
+		width: 500,
+		zIndex: 999,
+	},
+});
 
 export const Home: React.FC<{ userID: string }> = ({ userID }): JSX.Element => {
+	const classes = useStyles();
+
 	const [period, setPeriod] = useState(getStartPeriod());
 
 	const [yearIncome, setYearIncome] = useState([]);
@@ -19,6 +76,10 @@ export const Home: React.FC<{ userID: string }> = ({ userID }): JSX.Element => {
 
 	return (
 		<>
+		<div className={classes.messageSticky}>
+			{showMessage ? message : null}
+		</div>
+		<div className={classes.mainSection}>
 			<HomeHeader
 				handleYearChange={(e: { target: { value: string; }; }) => {
 					console.log(e.target.value)
@@ -35,6 +96,7 @@ export const Home: React.FC<{ userID: string }> = ({ userID }): JSX.Element => {
 					}
 				}}
 			/>
+		</div>
 		</>
 	);
 }
