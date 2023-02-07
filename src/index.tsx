7b3ee7630cbb78d8046f700d1f3034337c9e01e6
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-07 09:29:24 +0100

    Init typescript conversion

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'semantic-ui-css/semantic.min.css'
-import './index.css';
 import App from './App';
+import { JssProvider } from 'react-jss';
+
 import './firebase.js';
 import './sidebar/sidebar.css';
 import {Chart, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title, PointElement, LineElement} from 'chart.js'
 
 Chart.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title, PointElement, LineElement);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+root.render(
+  <JssProvider id={{ minify: true }}>
+    <App />
+  </JssProvider>
+);
+
 <link
-  async
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
 />
