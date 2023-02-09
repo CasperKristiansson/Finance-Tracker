c0081bb2f1291277f9ba05833741b8efc2899565
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-09 08:57:28 +0100

    add dep

--- src/Pages/Logout/Logout.tsx
+++ src/Pages/Logout/Logout.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export const Logout: React.FC<{ logout: any }> = ({ logout }): JSX.Element => {
 	useEffect(() => {
 		logout();
-	}, []);
+	}, [logout]);
 
 	return(
 		<>
