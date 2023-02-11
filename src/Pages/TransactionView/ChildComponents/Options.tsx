409e3de7fe30062bc4299aabbc591bd75bfb565a
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-11 09:08:13 +0100

    working table view

--- src/Pages/TransactionView/ChildComponents/Options.tsx
+++ src/Pages/TransactionView/ChildComponents/Options.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Form, Select, Container } from 'semantic-ui-react'
 import { sortOptions, transactionTypes } from '../../../Utils/Data/Table';
-import { dropDownStruct, TransactionCategory, TransactionTableOptions } from '../../../Utils/Miscellaneous';
+import { TransactionCategory, TransactionTableOptions } from '../../../Utils/Miscellaneous';
 
 export const Options: React.FC<{ options: TransactionTableOptions, setOptions: any, handleNewSearch: any, transactionCategories: TransactionCategory[] }> = ({ options, setOptions, handleNewSearch, transactionCategories }): JSX.Element => {
 	const [disTransactionCategories, setDisTransactionCategories] = useState([{ key: '', text: 'All', value: '' }]);
@@ -11,54 +11,50 @@ export const Options: React.FC<{ options: TransactionTableOptions, setOptions: a
 		else setOptions({ ...options, [name]: value });
 	}
 
-	// useEffect(() => {	
-	// 	if (transactionCategories.length > 1) {
-	// 		setDisTransactionCategories([...transactionCategories.filter((category: { type: string; }) => {
-	// 			if (options.transactionType === '') {
-	// 				return true;
-	// 			} else {
-	// 				return category.type.toLowerCase() === options.transactionType;
-	// 			}
-	// 		}).map((category: { type: string; category: string; }) => {
-	// 			return {
-	// 				key: category.type + '-' + category.category.toLowerCase(),
-	// 				text: category.category + ' (' + category.type + ')',
-	// 				value: category.category.toLowerCase()
-	// 			}
-	// 		})]);
-	// 	}
-	// }, [options.transactionType, transactionCategories])
-
-	useEffect(() => {
-		console.log(disTransactionCategories);
-	}, [disTransactionCategories])
+	useEffect(() => {	
+		if (transactionCategories.length > 1) {
+			setDisTransactionCategories([...transactionCategories.filter((category: { type: string; }) => {
+				if (options.transactionType === '') {
+					return true;
+				} else {
+					return category.type.toLowerCase() === options.transactionType;
+				}
+			}).map((category: { type: string; category: string; }) => {
+				return {
+					key: category.type + '-' + category.category.toLowerCase(),
+					text: category.category + ' (' + category.type + ')',
+					value: category.category.toLowerCase()
+				}
+			})]);
+		}
+	}, [options.transactionType, transactionCategories])
 
   return (
     <Container textAlign='center'>
       <Form>
         <Form.Group inline>
-          	{/* <Form.Field control={Select} label='Transaction Type'
+          	<Form.Field control={Select} label='Transaction Type'
 				options={transactionTypes} placeholder='Select transaction type'
-				name='transactionType' value={{key: "test", text: "test", value: "test"}}
-				onChange={handleChange}
-			/> */}
-          	<Form.Field control={Select} label='Transaction Category'
-				options={transactionCategories} placeholder='Select transaction category'
-				name='transactionCategory' value={{key: "test", text: "test", value: "test"}}
+				name='transactionType' value={options.transactionType}
 				onChange={handleChange}
 			/>
-          	{/* <Form.Field control={Select} label='Sort By' options={sortOptions}
-				placeholder='Select sort option' name='sort' value={options.sort}
+          	<Form.Field control={Select} label='Transaction Category'
+				options={disTransactionCategories} placeholder='Select transaction category'
+				name='transactionCategory' value={options.transactionCategory}
 				onChange={handleChange}
-			/> */}
-          	{/* <Form.Input label='Start Date' type='date' name='startDate'
+			/>
+          	<Form.Field control={Select} label='Sort By' options={sortOptions}
+				placeholder='Select sort option' name='sort' value={options.transactionSort}
+				onChange={handleChange}
+			/>
+          	<Form.Input label='Start Date' type='date' name='startDate'
 				value={options.startDate}
 				onChange={handleChange}
 			/>
           	<Form.Input label='End Date' type='date' name='endDate'
 				value={options.endDate}
 				onChange={handleChange}
-			/> */}
+			/>
         </Form.Group>
       </Form>
     </Container>
