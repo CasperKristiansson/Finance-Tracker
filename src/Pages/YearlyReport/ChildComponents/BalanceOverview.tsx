f83bd4668c024b47644f0336785117b319ab96a4
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-11 16:08:05 +0100

    Balance overview

--- src/Pages/YearlyReport/ChildComponents/BalanceOverview.tsx
+++ src/Pages/YearlyReport/ChildComponents/BalanceOverview.tsx
@@ -9,20 +9,20 @@ export const BalanceOverview: React.FC<{transactions: Transaction[] }> = ({ tran
 			<Grid.Row stretched>
 				<Grid.Column>
 					<Segment>
-						<h3 className="ui green header">Expense</h3>
-						<h1 className="ui green header">{FormatNumber(TotalTransactionType(transactions, "Expenses"))} kr</h1>
+						<h3 className="ui red header">Expense</h3>
+						<h1 className="ui red header">{FormatNumber(TotalTransactionType(transactions, "Expense"))} kr</h1>
 					</Segment>
 				</Grid.Column>
 				<Grid.Column>
 					<Segment>
-						<h3 className="ui red header">Income</h3>
-						<h1 className="ui red header">{FormatNumber(TotalTransactionType(transactions, "Income"))} kr</h1>
+						<h3 className="ui green header">Income</h3>
+						<h1 className="ui green header">{FormatNumber(TotalTransactionType(transactions, "Income"))} kr</h1>
 					</Segment>
 				</Grid.Column>
 				<Grid.Column>
 					<Segment>
 						<h3 className="ui blue header">Total Assets</h3>
-						<h1 className="ui blue header">{FormatNumber(TotalTransactionType(transactions, "Income") - TotalTransactionType(transactions, "Expenses"))} kr</h1>
+						<h1 className="ui blue header">{FormatNumber(TotalTransactionType(transactions, "Income") - TotalTransactionType(transactions, "Expense"))} kr</h1>
 					</Segment>
 				</Grid.Column>
 			</Grid.Row>
