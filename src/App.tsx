3d037cd612983895ce1311b618734b3570f043a8
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-03-08 22:06:43 +0100

    Spline import and basic login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router";
+import { Login } from "./pages/login/login";
 
 export const App: React.FC = () => {
-
   return (
-    // <Routes>
-    //   {/* <Route path="*" element={<NotFound />} /> */}
-    // </Routes>
-    <div>Hey</div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
 import { BrowserRouter } from "react-router";
+import { App } from "./app";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.querySelector("#root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
