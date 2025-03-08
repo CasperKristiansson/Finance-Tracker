c032ae4277ddb75def3ee2d35e2778eda81fec67
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-03-08 23:21:21 +0100

    Add navigation component and integrate it into the app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import { Navigation } from "./components/navigation/navigation";
 import { Cover } from "./pages/cover/cover";
 import { Login } from "./pages/login/login";
 import { NotFound } from "./pages/notFound/notFound";
 
 export const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/cover" element={<Cover />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/cover" element={<Cover />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
