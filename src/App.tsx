15b8c2c056bf91df7fba160d0f3efacc52b8f489
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-03-08 22:54:35 +0100

    Add NotFound page and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import { Cover } from "./pages/cover/cover";
 import { Login } from "./pages/login/login";
+import { NotFound } from "./pages/notFound/notFound";
 
 export const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/cover" element={<Cover />} />
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
