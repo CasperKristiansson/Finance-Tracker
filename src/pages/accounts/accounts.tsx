285bcd05295ffaf768ecb17bd617f752ebe808ea
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-03-09 17:05:49 +0100

    Add "Add Account" button to Accounts page and clean up navigation layout

--- src/pages/accounts/accounts.tsx
+++ src/pages/accounts/accounts.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { NetWorthPerformanceCard } from "./children/netWorth";
 import { AccountsCard } from "./children/accountCard";
 import { Summary } from "./children/summary";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 export const Accounts: React.FC = () => {
   return (
-    <div className="">
+    <div className="relative">
+      <div className="absolute top-0 right-0 mt-[-50px]">
+        <Button variant="default">
+          <Plus /> Add Account
+        </Button>
+      </div>
       <NetWorthPerformanceCard />
       <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3">
         <div className="col-span-2">
--- src/pages/navigation/navigation.tsx
+++ src/pages/navigation/navigation.tsx
@@ -33,10 +33,7 @@ export const Navigation: React.FC<{
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          {children}
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
