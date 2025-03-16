0feaace1d08f6bffb6e69c8b649bdb48bd6e62c7
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-03-16 14:07:05 +0100

    Add Sonner toast notifications for authentication errors and integrate next-themes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
+import { Toaster } from "@/components/ui/sonner";
 import { Store } from "./app/store.ts";
 import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={Store}>
     <BrowserRouter>
+      <Toaster />
       <App />
     </BrowserRouter>
   </Provider>,
