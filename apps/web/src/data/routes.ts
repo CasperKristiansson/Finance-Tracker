2d2c5a1bcadc333c2597ef668a0357a71d019599
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-10-03 18:48:25 +0200

    Update .gitignore, add Makefile, and include new image assets; define page routes

---
+++ apps/web/src/data/routes.ts
@@ -0,0 +1,14 @@
+export const PageRoutes = {
+  cover: "/cover",
+  login: "/login",
+  notFound: "*",
+  home: "/",
+  accounts: "/accounts",
+  transactions: "/transactions",
+  reports: "/reports",
+  cashFlow: "/cash-flow",
+  loans: "/loans",
+  investments: "/investments",
+  goals: "/goals",
+  settings: "/settings",
+};
