4eb4b56b53023bfa8e6ef0d1cde2fa0e1043b148
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-10-04 23:23:20 +0200

    feat: implement script to populate web app .env file with Cognito outputs from Terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,4 +3,9 @@ module "resources" {
   account_id     = var.account_id
   environment    = terraform.workspace
   enable_bastion = var.enable_bastion
+
+  providers = {
+    aws           = aws
+    aws.us_east_1 = aws.us_east_1
+  }
 }
